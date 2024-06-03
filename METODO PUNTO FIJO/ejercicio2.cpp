4704c2c425ce70fb924abb327bb7ed90a8b6525c
Orsade <63831224+davbr12@users.noreply.github.com>
2024-06-03 09:38:51 -0600

    ALGORITMO METODO DE PUNTO FIJO

---
+++ METODO PUNTO FIJO/ejercicio2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h> 
+#include <math.h> 
+
+using namespace std;
+
+int main()
+{
+    double  x,VAL1,VAL2,VAL3,NEWVAL,VALORANT, TOL, ERROR;
+    int N,OK = 1;
+    cout<<"BIENVENIDOS AL METODO DE PUNTO FIJO \n";
+    cout<<"Ingrese P0: \n";
+    cin>>x;
+    cout<<"Ingrese TOLERANCIA: \n";
+    cin>>TOL;
+    VALORANT = x;
+    cout<<"g(x) = ";
+    
+        for (int i = 0; i<= 100; i ++)
+        {
+            VAL1 = 10-pow(x,3);
+            VAL2 = VAL1/4;
+            VAL3 = sqrt(VAL2);
+            NEWVAL = VAL3;
+            ERROR = abs((NEWVAL-x)/NEWVAL);
+            x = NEWVAL;
+            if(ERROR<TOL){
+                i=100;
+                cout<<"APROXIMACION SOLUCION A LA RAIZ: ";
+                cout<<NEWVAL<<endl;
+            }
+        }
+}
